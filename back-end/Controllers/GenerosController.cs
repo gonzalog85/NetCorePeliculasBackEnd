f2be3121a5e86195384bd90312bcdd4d5b78433a
Gonzalo <gonzalogomez22@gmail.com>
2021-02-16 22:49:11 -0300

    Agregar archivos de proyecto.

---
+++ back-end/Controllers/GenerosController.cs
@@ -0,0 +1,58 @@
+﻿using back_end.Entidades;
+using back_end.Filtros;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenerosController: ControllerBase
+    {
+        private readonly ILogger<GenerosController> logger;
+
+        public GenerosController(ILogger<GenerosController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Genero>> Get()
+        {
+            return new List<Genero>() { new Genero() { Id = 1, Nombre = "Comedia"} };
+        }
+
+        [HttpGet("{Id:int}")] 
+        public async Task<ActionResult<Genero>> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
